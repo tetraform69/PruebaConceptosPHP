9bd22ca80ebda2d231af63c067ccff81c520f928
tetraform69 <oscorredor69@gmail.com>
2023-05-06 18:59:59 -0500

    Login Api runtime exeption

--- index.php
+++ index.php
@@ -4,16 +4,8 @@ include "user.php";
 
 $data = array();
 
-session_start();
-
 if (empty($_SESSION["user"]))
 {
     header("Location: login.php");
     exit();
 }
-
-$usuario = $_SESSION["user"];
-$data['status'] = 'ok';
-$data['message'] = 'You have Login';
-$data['result'] = $usuario->json();
-echo json_encode($data);
--- login.php
+++ login.php
@@ -15,31 +15,39 @@ $user = new user('Camilo', '9876', 'user');
 $users[$admin->getName()] = $admin;
 $users[$user->getName()] = $user;
 
-$json_string = file_get_contents('php://input');
-$data = json_decode($json_string);
+try {
+    throw new RuntimeException('El Json no contiene los datos necesarios: name, pasword');
 
-$name_consulta = $data->name;
-$pasword_consulta = $data->pasword;
-
-if (!array_key_exists($name_consulta, $users))
-{
-    $json['status'] = 'error';
-    $json['result'] = 'No existe un usuario con esos datos';
-    echo json_encode($json);
-} 
-else 
-{
-    $usuario = $users[$name_consulta];
-    if ($usuario->getPasword() != $pasword_consulta)
+    $json_string = file_get_contents('php://input');
+    $data = json_decode($json_string);
+    
+    $name_consulta = $data->name;
+    $pasword_consulta = $data->pasword;
+    
+    if (!array_key_exists($name_consulta, $users))
     {
         $json['status'] = 'error';
-        $json['result'] = 'contraseña incorrecta';
-        echo json_encode($json);
-    }
-    else
+        $json['result'] = 'No existe un usuario con esos datos';
+    } 
+    else 
     {
-        session_start();
-        $_SESSION["user"] = $usuario;
-        header($rol_site[$usuario->getRol()]);
+        $usuario = $users[$name_consulta];
+        if ($usuario->getPasword() != $pasword_consulta)
+        {
+            $json['status'] = 'error';
+            $json['result'] = 'contraseña incorrecta';
+        }
+        else
+        {
+            session_start();
+            $_SESSION["user"] = $usuario;
+            $json['status'] = 'ok';
+            $json['message'] = 'You have Login';
+            $json['result'] = $usuario->json();
+        }
     }
+} catch (RuntimeException $e) {
+    $json['status'] = 'error';
+    $json['result'] = $e->getMessage();
 }
+echo json_encode($json);
