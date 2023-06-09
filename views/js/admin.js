aa5bd638b49db5591294e164d87f3e70d0b1cbdf
tetraform69 <oscorredor69@gmail.com>
2023-06-08 20:01:54 -0500

    el admin ve una modal para eliminar falta que pueda editar su propia informacion

--- views/js/admin.js
+++ views/js/admin.js
@@ -1,10 +1,12 @@
 modalContainer = document.getElementById(`modal-container`)
-modalContainer.addEventListener('click', () => {
-    modalContainer.style.visibility = "hidden"
-    let modals = document.querySelectorAll('div.modal')
-    modals.forEach(modal => {
-        modal.style.display = "none"
-    });
+modalContainer.addEventListener('click', (event) => {
+    if (event.target == modalContainer) {
+        modalContainer.style.visibility = "hidden"
+        let modals = document.querySelectorAll('div.modal')
+        modals.forEach(modal => {
+            modal.style.display = "none"
+        });
+    }
 })
 
 function openModal(id) {
